e68300532ab8d377ce92eaeb0dadea4a4d903e9f
yang <theonlyprince@126.com>
2025-04-16 15:35:57 +0200

    Pixl optimizing.

--- filters/blurfilter.c
+++ filters/blurfilter.c
@@ -8,10 +8,46 @@
 #include "blurfilter.h"
 #include "ppmio.h"
 
+const int ThreadCount = 1024 * 1024;
+
 pixel* pix(pixel* image, const int xx, const int yy, const int xsize)
 {
 	int off = xsize*yy + xx;
 	return (image + off);
+}
+
+void pix_handle_thread(int x, int y, const int xsize, const int ysize, pixel* src, pixel* dst, double *w, int mode) {
+	r = w[0] * pix(src, x, y, xsize)->r;
+	g = w[0] * pix(src, x, y, xsize)->g;
+	b = w[0] * pix(src, x, y, xsize)->b;
+	n = w[0];
+	for ( wi=1; wi <= radius; wi++)
+	{
+		wc = w[wi];
+		if (mode == 0) x2 = x - wi;
+		else y2 = y - wi;
+		
+		if (x2 >= 0 && y2 >= 0)
+		{
+			r += wc * pix(src, x2, y2, xsize)->r;
+			g += wc * pix(src, x2, y2, xsize)->g;
+			b += wc * pix(src, x2, y2, xsize)->b;
+			n += wc;
+		}
+		if (mode == 0) x2 = x + wi;
+		else y2 = y + wi;
+		if (x2 < xsize && y2 < ysize)
+		{
+			r += wc * pix(src, x2, y2, xsize)->r;
+			g += wc * pix(src, x2, y2, xsize)->g;
+			b += wc * pix(src, x2, y2, xsize)->b;
+			n += wc;
+		}
+	}
+	pix(dst,x,y, xsize)->r = r/n;
+	pix(dst,x,y, xsize)->g = g/n;
+	pix(dst,x,y, xsize)->b = b/n;
+
 }
 
 void blurfilter(const int xsize, const int ysize, pixel* src, int startRow, int endRow, const int radius, const double *w)
@@ -19,73 +55,34 @@ void blurfilter(const int xsize, const int ysize, pixel* src, int startRow, int 
 	int x, y, x2, y2, wi;
 	double r, g, b, n, wc;
 	pixel *dst = (pixel*) malloc(sizeof(pixel) * MAX_PIXELS);
+
+	pthread_t threads[ThreadCount];
+	int threadId = 0;
 	
-	for (y=startRow; y<endRow; y++)
+	for (y=0; y<ysize; y++)
 	{
 		for (x=0; x<xsize; x++)
 		{
-			r = w[0] * pix(src, x, y, xsize)->r;
-			g = w[0] * pix(src, x, y, xsize)->g;
-			b = w[0] * pix(src, x, y, xsize)->b;
-			n = w[0];
-			for ( wi=1; wi <= radius; wi++)
-			{
-				wc = w[wi];
-				x2 = x - wi;
-				if (x2 >= 0)
-				{
-					r += wc * pix(src, x2, y, xsize)->r;
-					g += wc * pix(src, x2, y, xsize)->g;
-					b += wc * pix(src, x2, y, xsize)->b;
-					n += wc;
-				}
-				x2 = x + wi;
-				if (x2 < xsize)
-				{
-					r += wc * pix(src, x2, y, xsize)->r;
-					g += wc * pix(src, x2, y, xsize)->g;
-					b += wc * pix(src, x2, y, xsize)->b;
-					n += wc;
-				}
-			}
-			pix(dst,x,y, xsize)->r = r/n;
-			pix(dst,x,y, xsize)->g = g/n;
-			pix(dst,x,y, xsize)->b = b/n;
+			pthread_create(&threads[threadId], NULL, pix_handle_thread, (void*)pix(x, y, xsize, ysize, src, dst, w, 0));
+			threadId++;
 		}
 	}
+
+	for (int t = 0; t < threadId; t++) {
+        pthread_join(threads[t], NULL);
+    }
+	threadId = 0;
 	
-	for (y=startRow; y<endRow; y++)
+	for (y=0; y<ysize; y++)
 	{
 		for (x=0; x<xsize; x++)
 		{
-			r = w[0] * pix(dst, x, y, xsize)->r;
-			g = w[0] * pix(dst, x, y, xsize)->g;
-			b = w[0] * pix(dst, x, y, xsize)->b;
-			n = w[0];
-			for ( wi=1; wi <= radius; wi++)
-			{
-				wc = w[wi];
-				y2 = y - wi;
-				if (y2 >= 0)
-				{
-					r += wc * pix(dst, x, y2, xsize)->r;
-					g += wc * pix(dst, x, y2, xsize)->g;
-					b += wc * pix(dst, x, y2, xsize)->b;
-					n += wc;
-				}
-				y2 = y + wi;
-				if (y2 < ysize)
-				{
-					r += wc * pix(dst, x, y2, xsize)->r;
-					g += wc * pix(dst, x, y2, xsize)->g;
-					b += wc * pix(dst, x, y2, xsize)->b;
-					n += wc;
-				}
-			}
-			pix(src,x,y, xsize)->r = r/n;
-			pix(src,x,y, xsize)->g = g/n;
-			pix(src,x,y, xsize)->b = b/n;
+			pthread_create(&threads[threadId], NULL, pix_handle_thread, (void*)pix(x, y, xsize, ysize, src, dst, w, 0));
+			threadId++;	
 		}
 	}
+	for (int t = 0; t < threadId; t++) {
+        pthread_join(threads[t], NULL);
+    }
 	free(dst);
 }
